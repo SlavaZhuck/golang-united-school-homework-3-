20b013804fcb8bcfc6b236a3787979fe9c7fda38
Viachaslau_Zhuk <Zhukviachaslau@gmail.com>
2022-05-06 20:38:49 +0300

    task 2 init version

--- task02-slice.go
+++ task02-slice.go
@@ -1,6 +1,21 @@
 package homework
 
+// package main
+
 func reverse(input []int64) (result []int64) {
 	//Place your code here
-	return
+	s := make([]int64, len(input))
+	for i := 0; i < len(input); i++ {
+		s[i] = input[len(input)-i-1]
+	}
+	return s
 }
+
+// func main() {
+
+// 	primes := [6]int64{2, 3, 5, 7, 11, 13}
+
+// 	var s []int64 = primes[0:6]
+
+// 	fmt.Println(reverse(s))
+// }
